ea9b1f4483232c733cc649c974ce8011fef4a404
Manuel2u <edodoo011@st.ug.edu.gh>
2022-11-14 12:14:23 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "340px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        primary: "#00FFE1",
+        secondary: "#898989",
+        tertiary: "#28365f",
+        deep: "#2b55cc",
+        darktxt: "#848db3",
+        darkBlue: "#151a33",
+        skills: "#F2F2F2",
+        projects: "#161616",
+        darknight: "#252A2F",
+      },
+      fontFamily: {
+        dodoo: ["Sacramento"],
+      },
+    },
+  },
+  plugins: [],
+};
